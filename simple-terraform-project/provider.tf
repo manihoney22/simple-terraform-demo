a2e8ea58756fd13e111275c4a1ef57ee87db7cc2
Manikantan <91248096+manihoney22@users.noreply.github.com>
2024-06-05 21:59:51 +0530

    Update provider.tf

--- simple-terraform-project/provider.tf
+++ simple-terraform-project/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIASI6K7KRQG7LM4PZE"
-  secret_key = "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"
+  access_key = "AKIA3FLDXKK77XDWH5HT"
+  secret_key = "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"
 }
