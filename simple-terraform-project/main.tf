a15d74dd60fcf247ac23e01ec2c0153d090f4bef
Manikantan <91248096+manihoney22@users.noreply.github.com>
2024-06-05 22:02:10 +0530

    Update main.tf

--- simple-terraform-project/main.tf
+++ simple-terraform-project/main.tf
@@ -1,10 +1,10 @@
 #create s3 bucket
-resource "aws_s3_bucket" "mybucket" {
+resource "aws_s3_bucket" "myfirstbucket" {
   bucket = var.bucketname
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.mybucket.id
+  bucket = aws_s3_bucket.myfirstbucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -12,12 +12,12 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.mybucket.id
+  bucket = aws_s3_bucket.myfirstbucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "example" {
@@ -27,14 +27,14 @@ resource "aws_s3_bucket_acl" "example" {
   ]
 
   bucket = aws_s3_bucket.mybucket.id
-  acl    = "private"
+  acl    = "public-read"
 }
 
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
   source = "index.html"
-  acl = "private"
+  acl = "public-read"
   content_type = "text/html"
 }
 
