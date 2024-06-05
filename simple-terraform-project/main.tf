684b088b7264fe848e5a0738054374eba249ce02
Manikantan <91248096+manihoney22@users.noreply.github.com>
2024-06-05 14:35:39 +0530

    Update main.tf

--- simple-terraform-project/main.tf
+++ simple-terraform-project/main.tf
@@ -14,10 +14,10 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "example" {
@@ -27,14 +27,14 @@ resource "aws_s3_bucket_acl" "example" {
   ]
 
   bucket = aws_s3_bucket.mybucket.id
-  acl    = "public-read"
+  acl    = "private"
 }
 
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
   source = "index.html"
-  acl = "public-read"
+  acl = "private"
   content_type = "text/html"
 }
 
