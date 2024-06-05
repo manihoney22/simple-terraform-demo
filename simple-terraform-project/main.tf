a0f027daa5cd1d4e50a4f7f654098495a97d4d01
Manikantan <91248096+manihoney22@users.noreply.github.com>
2024-06-05 14:33:05 +0530

    Update main.tf

--- simple-terraform-project/main.tf
+++ simple-terraform-project/main.tf
@@ -38,14 +38,6 @@ resource "aws_s3_object" "index" {
   content_type = "text/html"
 }
 
-resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.mybucket.id
-  key = "error.html"
-  source = "error.html"
-  acl = "public-read"
-  content_type = "text/html"
-}
-
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
   index_document {
